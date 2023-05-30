aa24c3323e703f3001d639da52b071245f288213
gitmyth <115619548+gitmyth@users.noreply.github.com>
2023-05-30 04:50:14 +0000

    added api

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Bengaluru, India',
+  'salary': 'Rs. 10,00,000'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Chennai, India',
+  'salary': 'Rs. 8,00,000'
+}, {
+  'id': 3,
+  'title': 'BackEnd Engineer',
+  'location': 'New York, USA',
+  'salary': '$ 120,000'
+}, {
+  'id': 4,
+  'title': 'FrontEnd Engineer',
+  'location': 'Remote',
+}]
 
 
 @app.route("/")
 def hello_world():
-  return "Hello World"
+  return render_template("home.html", jobs=JOBS, company_name="Flask Test")
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
